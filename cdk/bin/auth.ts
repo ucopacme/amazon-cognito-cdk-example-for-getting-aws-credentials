87c2bd05d9c9bfa8b095aa8970355f8e3f53fe74
Eric Odell <eric.odell@ucop.edu>
2020-06-15 11:19:28 -0700

    add ucop standards to cdk

---
+++ cdk/bin/auth.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import infra = require("@ucop-acme/aws-infra");
+import config from "../config.json";
+import { Auth } from '../lib/auth';
+
+const codebuildUrl: string = process.env.CODEBUILD_BUILD_URL || "manual";
+
+const Tags = config[`tags`];
+const stacks = config[`stacks`];
+const branchEnv = infra.mapBranchToEnvironment().trim();
+Tags.environment = branchEnv;
+const accounts = config.accounts;
+const branchAccount = accounts.find((accnt) => accnt.env === branchEnv);
+const stackEnv = {
+  account: branchAccount!.account,
+  region: branchAccount!.region,
+};
+const myStackDescription =
+  Tags.application +
+  " " +
+  Tags.environment +
+  " " +
+  Tags.description +
+  infra.makeBrand();
+const myStackProps: infra.IBaseStackProps = {
+  description: myStackDescription,
+  env: stackEnv,
+  baseprops: {
+    application: Tags.application,
+    buildId: codebuildUrl,
+    createdBy: Tags.createdBy,
+    description: Tags.description,
+    environment: Tags.environment,
+    group: Tags.group,
+    label: "",
+    source: Tags.source + ":" + Tags.environment,
+  },
+};
+const app = new cdk.App();
+infra.tagApp(app, myStackProps);
+myStackProps.baseprops.label = stacks[`auth`][`label`];
+new Auth(app, myStackProps);
---
+++ cdk/lib/auth.ts
@@ -0,0 +1,12 @@
+import * as cdk from '@aws-cdk/core';
+import infra = require("@ucop-acme/aws-infra");
+import cognito = require("../lib/cognito");
+
+export class Auth extends infra.BaseStack {
+  constructor(scope: cdk.App, props: infra.IBaseStackProps) {
+    super(scope, props);
+
+    new cognito.CognitoService(this, "Auth")
+
+  }
+}
