52a31aa69a0cb7eb1b9f8080b2b06e7b6397106b
Eric Odell <eric.odell@ucop.edu>
2020-07-23 11:27:41 -0700

    branding

--- ui-react/src/config/appConfig.js
+++ ui-react/src/config/appConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   //**UI-REACT APP SETTINGS**
-  title: "Proudly built in Oakland, Ca. by the UCOP Architecture Corporation, patent pending",
-  signInHeader: "UCOP Student Data Analytics Platform token vending",
-  groupPageHeader: "UCOP Student Data Analytics Platform token vending",
+  title: "Proudly built in Oakland, Ca. by the UCOP Architecture Team, patent pending",
+  signInHeader: "UCOP Token Vending System",
+  groupPageHeader: "UCOP Token Vending  System",
 };
